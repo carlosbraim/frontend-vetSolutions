df33da22c9b2f4dc1669df46aba797edaad5da7a
Carlos Júnior <carlosbraim2010@gmail.com>
2024-03-14 12:59:24 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3004/',
+    baseURL: 'https://backend-vet-solutions-4tvf.vercel.app/',
     headers : {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
