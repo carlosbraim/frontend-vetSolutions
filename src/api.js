5f773fa7aecbcd6cf7513395f76f6df7d7f2130d
Carlos Júnior <carlosbraim2010@gmail.com>
2024-03-17 22:34:58 -0300

    testes

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://backend-vet-solutions.vercel.app/',
+    baseURL: 'https://backend-vetsolutions-1.onrender.com/',
     headers : {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
